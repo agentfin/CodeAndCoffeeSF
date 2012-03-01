3eec509edcc59571c0f0b2c3359c40aec85b1841
alexis finch <agent.fin@gmail.com>
2012-03-01 08:10:41 -0800

    A Scaffolding to Grow Upon

---
+++ coffee_scaffold/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CoffeeScaffold::Application.config.secret_token = 'baba67e17855e88b5a4d749b152b4c59ee0904ace99f1a209d86484e0b21b6b9f181feae4624a450b9ff3cf7e31e530dc7ff63ce335ce1daebe2aca8bc3764f7'
